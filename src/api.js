caeffc27b0bee26d0fdb1a3ce89dc8fab3f904f1
Pedro Gardete <A47218@alunos.isel.pt>
2022-08-20 17:24:13 +0100

    Changes to compute_metrics
    Separates the compute function from the metric implementations. Now checks metrics array and if the available conditions are met to be able to compute.

--- src/api.js
+++ src/api.js
@@ -5,7 +5,9 @@ import keyboardManager from './keyboardManager.js'
 import FileSaver from 'file-saver' ;
 var BitSet = require('bitset')
 var HashMap = require('hashmap')
+var default_metrics = require('./metric_modules/default_metrics')
 // class to handle user interaction to init and set up phyloIO instance
+
 export default class API { // todo ultime ! phylo is used ase reference from .html not goood
 
     constructor() {
@@ -14,11 +16,6 @@ export default class API { // todo ultime ! phylo is used ase reference from .ht
         this.session_token = null
         this.session_url = null
         this.phylo_embedded = false
-        this.available_metrics = {
-            'RDF' : false,
-            'WRF' : false,
-            'GRF' : false
-        }
         this.leaf_info = {
             'intersect' : false,
             'include' : false
@@ -49,9 +46,8 @@ export default class API { // todo ultime ! phylo is used ase reference from .ht
             "compareMode" : false, // compare for each pair of tree topological similarity
         };
         this.undoing = false;
-        this.id_set = {}
-
-
+        this.id_set = {};
+        this.metrics = default_metrics.metrics
     }
 
     reset(){ // !!!! KEEP ATTR UPDATED BETWEEN init and reset TODO AUTO THAT
@@ -91,6 +87,8 @@ export default class API { // todo ultime ! phylo is used ase reference from .ht
         this.undoing = false;
 
     }
+
+
 
     // create and return a new container and add it the dict using its div id
     create_container(container_id){ // container_id -> str
@@ -233,6 +231,35 @@ export default class API { // todo ultime ! phylo is used ase reference from .ht
         var mod1 = this.bound_container[0].models[this.bound_container[0].current_model]
         var mod2 = this.bound_container[1].models[this.bound_container[1].current_model]
 
+        const available_metrics = this.metrics.filter(metric => {
+                const filtered_conditions = metric.conditions.filter(condition => {
+                    if (this.leaf_info.hasOwnProperty(condition)) {
+                        return this.leaf_info[condition]
+                    }
+                    return false
+                })
+                return metric.conditions.length === filtered_conditions.length
+            }
+        )
+
+        available_metrics.forEach(metric => {
+            const result = metric.compute(mod1,mod2,this.id_set)
+            this.distance[metric.name] = result
+        } )
+
+        /*
+        this.metrics.forEach(metric => {
+            if(!metric.conditions.filter(condition =>{
+                    if(this.leaf_info.hasOwnProperty(condition)){
+                        return !this.leaf_info[condition]
+                    }
+                    return true
+            })){
+                const result = metric.compute(mod1,mod2,this.id_set)
+                this.distance[metric.name] = result
+            }
+        })
+         */
 
         //var leaves1 = mod1.hierarchy_mockup.leaves().map(x => x.data.name);
         //var leaves2 = mod2.hierarchy_mockup.leaves().map(x => x.data.name);
@@ -256,107 +283,7 @@ export default class API { // todo ultime ! phylo is used ase reference from .ht
         //mod1.createDeepLeafList()
 
 
-        /*
-        * We decide which metrics can be calculated based on their leaf information
-        * R&F and Weighed R&F can be calculated if the leaf sets of both trees intersect themselves
-        * Generalized R&F can be calculated if the leaf sets of both trees don't intersect themselves and one of those
-        * sets is included in the other
-        * */
-        if(this.available_metrics.RDF && this.leaf_info.intersect) {
-            this.distance.RF = compute_RF_distance(mod1, mod2, this.id_set)
-        }
 
-        if(this.available_metrics.WRF && this.leaf_info.intersect) {
-            this.distance.WRF = compute_WRF_distance(mod1, mod2,this.id_set)
-        }
-
-        if(this.available_metrics.GRF && this.leaf_info.include && !this.leaf_info.intersect ) {
-            this.distance.GRF = compute_GRF_distance(mod1, mod2, this.id_set)
-        }
-
-        function compute_RF_distance(mod1,mod2, id_set) {
-
-            var cl_tree1 = mod1.get_clusters_rf(id_set)
-            var cl_tree2 = mod2.get_clusters_rf(id_set)
-
-            var shared_clusters = 0
-            const total_clusters = cl_tree1.size + cl_tree2.size
-
-            /*
-            * Comparing both cluster maps to check how similar they are
-            * */
-            var iterator = cl_tree2.values()
-            cl_tree1.forEach(function (value, key) {
-                for (const bitset of iterator) {
-                    if(bitset.equals(value)){
-                        shared_clusters++
-                        break
-                    }
-                }
-            })
-
-            var rf_distance = -1
-
-            /*
-            * The number of shared clusters should not be bigger than the number of clusters of both trees
-            * */
-            if(shared_clusters * 2 < total_clusters ) {
-                rf_distance = (total_clusters - shared_clusters * 2) / 2
-            }
-
-            return rf_distance
-        }
-
-        function compute_WRF_distance(mod1,mod2, id_set) {
-
-            let distance_map = new HashMap()
-
-            var cl_tree1 = mod1.get_clusters_wrf(id_set,distance_map)
-            var cl_tree2 = mod2.get_clusters_wrf(id_set,distance_map)
-
-            var wrf_distance = 0
-
-            distance_map.forEach(function (value, key) {
-                wrf_distance += Math.abs(value)
-            })
-
-            return wrf_distance
-        }
-
-        function compute_GRF_distance(mod1,mod2, id_set) {
-
-            var cl_tree1 = mod1.get_clusters_rf(id_set)
-            var cl_tree2 = mod2.get_clusters_rf(id_set)
-            var max_shared_clusters = cl_tree1.size
-
-            if(cl_tree1.size > cl_tree2.size) {
-                max_shared_clusters = cl_tree2.size
-                let temp = cl_tree2
-                cl_tree2 = cl_tree1
-                cl_tree1 = temp
-            }
-
-            var shared_clusters = 0
-            const total_clusters = cl_tree1.size + cl_tree2.size
-
-            var iterator = cl_tree2.values()
-            cl_tree1.forEach(function (value, key) {
-                for (const bitset of iterator) {
-                    if(bitset.equals(value)){
-                        shared_clusters++
-                        break
-                    }
-                }
-            })
-
-            var grf_distance = -1
-
-            if(shared_clusters <= max_shared_clusters ) {
-                grf_distance = (total_clusters - shared_clusters*2) / 2
-            }
-
-            return grf_distance
-        }
     }
 
 
---
+++ src/metric_modules/default_metrics.js
@@ -0,0 +1,143 @@
+const HashMap = require("hashmap");
+
+
+const metrics = []
+/*
+* We decide which metrics can be calculated based on their leaf information
+* R&F and Weighed R&F can be calculated if the leaf sets of both trees intersect themselves
+* Generalized R&F can be calculated if the leaf sets of both trees don't intersect themselves and one of those
+* sets is included in the other
+* */
+
+metrics.push(
+    {
+        name: "RDF",
+        compute: compute_RF_distance,
+        description: "RDF Metric",
+        conditions: ["intersect"],
+        selected: false,
+        ref: "link"
+    }
+)
+
+metrics.push(
+    {
+        name: "WRF",
+        compute: compute_WRF_distance,
+        description: "WRF Metric",
+        conditions: ["intersect"],
+        selected: false,
+        ref: "link"
+    }
+)
+
+metrics.push(
+    {
+        name: "GRF",
+        compute: compute_GRF_distance,
+        description: "GRF Metric",
+        conditions: ["include"],
+        selected: false,
+        ref: "link"
+    }
+)
+
+export {metrics}
+
+/*
+if(this.available_metrics.RDF && this.leaf_info.intersect) {
+    this.distance.RF = compute_RF_distance(mod1, mod2, this.id_set)
+}
+
+if(this.available_metrics.WRF && this.leaf_info.intersect) {
+    this.distance.WRF = compute_WRF_distance(mod1, mod2,this.id_set)
+}
+
+if(this.available_metrics.GRF && this.leaf_info.include && !this.leaf_info.intersect ) {
+    this.distance.GRF = compute_GRF_distance(mod1, mod2, this.id_set)
+}
+ */
+
+function compute_RF_distance(mod1, mod2, id_set) {
+
+    var cl_tree1 = mod1.get_clusters_rf(id_set)
+    var cl_tree2 = mod2.get_clusters_rf(id_set)
+
+    var shared_clusters = 0
+    const total_clusters = cl_tree1.size + cl_tree2.size
+
+    /*
+    * Comparing both cluster maps to check how similar they are
+    * */
+    var iterator = cl_tree2.values()
+    cl_tree1.forEach(function (value, key) {
+        for (const bitset of iterator) {
+            if (bitset.equals(value)) {
+                shared_clusters++
+                break
+            }
+        }
+    })
+
+    var rf_distance = -1
+
+    /*
+    * The number of shared clusters should not be bigger than the number of clusters of both trees
+    * */
+    if (shared_clusters * 2 < total_clusters) {
+        rf_distance = (total_clusters - shared_clusters * 2) / 2
+    }
+
+    return rf_distance
+}
+
+function compute_WRF_distance(mod1, mod2, id_set) {
+
+    let distance_map = new HashMap()
+
+    var cl_tree1 = mod1.get_clusters_wrf(id_set, distance_map)
+    var cl_tree2 = mod2.get_clusters_wrf(id_set, distance_map)
+
+    var wrf_distance = 0
+
+    distance_map.forEach(function (value, key) {
+        wrf_distance += Math.abs(value)
+    })
+
+    return wrf_distance
+}
+
+function compute_GRF_distance(mod1, mod2, id_set) {
+
+    var cl_tree1 = mod1.get_clusters_rf(id_set)
+    var cl_tree2 = mod2.get_clusters_rf(id_set)
+    var max_shared_clusters = cl_tree1.size
+
+    if (cl_tree1.size > cl_tree2.size) {
+        max_shared_clusters = cl_tree2.size
+        let temp = cl_tree2
+        cl_tree2 = cl_tree1
+        cl_tree1 = temp
+    }
+
+    var shared_clusters = 0
+    const total_clusters = cl_tree1.size + cl_tree2.size
+
+    var iterator = cl_tree2.values()
+    cl_tree1.forEach(function (value, key) {
+        for (const bitset of iterator) {
+            if (bitset.equals(value)) {
+                shared_clusters++
+                break
+            }
+        }
+    })
+
+    var grf_distance = -1
+
+    if (shared_clusters <= max_shared_clusters) {
+        grf_distance = (total_clusters - shared_clusters * 2) / 2
+    }
+
+    return grf_distance
+}
